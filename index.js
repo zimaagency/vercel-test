2cc03584602dc00ae46acbae27aa52420b038c5d
Alex Ardevol <aardevol@omnios.ai>
2023-07-18 12:04:24 +0200

    re-initial

--- index.js
+++ index.js
@@ -1,124 +1,144 @@
-const app = require("express")();
+// Import necessary packages and initialize Express
+const express = require("express");
 const fs = require('fs-extra');
 const PDFDocument = require('pdfkit');
-import aws from 'aws-sdk';
+const aws = require('aws-sdk');
 
-let chrome = {};
 let puppeteer;
+let chrome = {};
 
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  puppeteer = require("puppeteer");
+// Function to set up Puppeteer based on environment
+function setupPuppeteer() {
+  // Check if the script is running on AWS Lambda
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    // Use 'chrome-aws-lambda' and 'puppeteer-core' for Lambda
+    chrome = require("chrome-aws-lambda");
+    puppeteer = require("puppeteer-core");
+  } else {
+    // Use 'puppeteer' otherwise
+    puppeteer = require("puppeteer");
+  }
 }
 
+// Function to generate PDF from given HTML content
+async function generatePDF(options, config, html) {
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
 
+  // Set page details
+  const { width, height, margin } = config;
+  await page.setViewport({
+    width: parseInt(width),
+    height: parseInt(height),
+    deviceScaleFactor: 1,
+  });
+
+  // Set extra headers
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'en-US',
+  });
+
+  // Set content and generate PDF
+  await page.setContent(html);
+  const pdf = await page.pdf(config);
+
+  // Close the browser
+  await browser.close();
+
+  return pdf;
+}
+
+// Function to write PDF file to AWS S3
+function writeToS3(pdf, timestamp, res) {
+  // Prepare to write to AWS S3
+  const params = {
+    Key: `real_${timestamp}.pdf`,
+    Body: pdf,
+    Bucket: 'bucket-name',
+    ContentType: 'application/pdf',
+  };
+
+  // AWS S3 configuration
+  const s3 = new aws.S3({
+    accessKeyId: process.env.YOUR_AWS_KEY,
+    secretAccessKey: process.env.YOUR_AWS_SECRET,
+  });
+
+  // Write to S3
+  s3.putObject(params, function (err, response) {
+    if(err) {
+      console.log(err);
+      res.status(500).json({ response: `Error writing file ${timestamp} to S3` });
+    } else {
+      res.status(200).json({ response: `File ${timestamp} saved to S3` });
+    }
+  });
+}
+
+// Initialize express
+const app = express();
+
+// Define API endpoint
 app.get("/api", async (req, res) => {
+  setupPuppeteer();
+  
   let options = {};
 
+  // Set options if running on AWS Lambda
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      args: chrome.args.concat(["--hide-scrollbars", "--disable-web-security"]),
       defaultViewport: chrome.defaultViewport,
       executablePath: await chrome.executablePath,
-      headless: true,
+      headless: chrome.headless,
       ignoreHTTPSErrors: true,
     };
   }
 
-    const bodyjson = {
-      "config": {
-        "width": "11.694444444444445in",
-        "height": "8.26388888888889in",
-        "margin": {
-          "top": "0in",
-          "right": "0.20833333333333334in",
-          "bottom": "0in",
-          "left": "0.20833333333333334in"
-        },
-        "displayHeaderFooter": true,
-        "headerTemplate": " ",
-        "footerTemplate": " ",
-        "printBackground": true,
-        "viewport": {
-          "width": 686,
-          "height": 976
-        },
-        "scale": 1.578231292517007
+  // PDF configuration
+  const bodyjson = {
+    "config": {
+      "width": "11.694444444444445in",
+      "height": "8.26388888888889in",
+      "margin": {
+        "top": "0in",
+        "right": "0.20833333333333334in",
+        "bottom": "0in",
+        "left": "0.20833333333333334in"
       },
-        "html": "test"
-    }
-
-    const { config, html } = bodyjson;
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
-    // Set the page dimensions and margin
-    const { width, height, margin } = config;
-    await page.setViewport({
-      width: parseInt(width),
-      height: parseInt(height),
-      deviceScaleFactor: 1,
-    });
-    await page.setExtraHTTPHeaders({
-      'Accept-Language': 'en-US',
-    });
-
-    // Set the page content with the provided HTML
-    await page.setContent(html);
-    // Generate the PDF with the specified configuration
-    const pdf = await page.pdf({
-      format: 'A4',
-      margin: {
-        top: parseFloat(margin.top),
-        right: parseFloat(margin.right),
-        bottom: parseFloat(margin.bottom),
-        left: parseFloat(margin.left),
+      "displayHeaderFooter": true,
+      "headerTemplate": " ",
+      "footerTemplate": " ",
+      "printBackground": true,
+      "viewport": {
+        "width": 686,
+        "height": 976
       },
-      displayHeaderFooter: config.displayHeaderFooter,
-      headerTemplate: config.headerTemplate,
-      footerTemplate: config.footerTemplate,
-      printBackground: config.printBackground,
-      scale: config.scale,
-    });
-
-    // Close the browser
-    await browser.close();
-    
-    const timestamp = Date.now();
-    const doc = new PDFDocument();
-    //use the tmp serverless function folder to create the write stream for the pdf
-    let writeStream = fs.createWriteStream(`/tmp/${timestamp}.pdf`);
-    doc.pipe(writeStream);
-    doc.text('title');
-    doc.end();
-
-    writeStream.on('finish', function () {
-      //once the doc stream is completed, read the file from the tmp folder
-      const fileContent = pdf;
-      //create the params for the aws s3 bucket
-      var params = {
-        Key: `real_${timestamp}.pdf`,
-        Body: fileContent,
-        Bucket: 'bubble-upload-s3-bucket',
-        ContentType: 'application/pdf',
-      };
+      "scale": 1.578231292517007
+    },
+      "html": "test"
+  }
   
-      //Your AWS key and secret pulled from environment variables
-      const s3 = new aws.S3({
-        accessKeyId: process.env.YOUR_AWS_KEY,
-        secretAccessKey: process.env.YOUR_AWS_SECRET,
-      });
+  const { config, html } = bodyjson;
   
+  const pdf = await generatePDF(options, config, html);
   
-      s3.putObject(params, function (err, response) {
-        res.status(200).json({ response: `File ${timestamp} saved to S3` });
-      });
-    });
+  // Generate timestamp for unique file names
+  const timestamp = Date.now();
 
-    
+  // Initialize a new PDF document
+  const doc = new PDFDocument();
 
+  // Create write stream for the new PDF
+  let writeStream = fs.createWriteStream(`/tmp/${timestamp}.pdf`);
+  doc.pipe(writeStream);
+  doc.text('title');
+  doc.end();
+
+  // Once write stream is finished
+  writeStream.on('finish', function () {
+    writeToS3(pdf, timestamp, res);
+  });
 });
 
 module.exports = app;
