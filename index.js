78b6daf58362e3a2e59f740c7fa814ddde08b84c
Alex Ardevol <aardevol@omnios.ai>
2023-07-18 12:05:28 +0200

    re-initial

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Import necessary packages and initialize Express
 const express = require("express");
 const fs = require('fs-extra');
 const PDFDocument = require('pdfkit');
@@ -7,25 +6,22 @@ const aws = require('aws-sdk');
 let puppeteer;
 let chrome = {};
 
-// Function to set up Puppeteer based on environment
+
 function setupPuppeteer() {
-  // Check if the script is running on AWS Lambda
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    // Use 'chrome-aws-lambda' and 'puppeteer-core' for Lambda
     chrome = require("chrome-aws-lambda");
     puppeteer = require("puppeteer-core");
   } else {
-    // Use 'puppeteer' otherwise
     puppeteer = require("puppeteer");
   }
 }
 
-// Function to generate PDF from given HTML content
+
 async function generatePDF(options, config, html) {
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
 
-  // Set page details
+
   const { width, height, margin } = config;
   await page.setViewport({
     width: parseInt(width),
@@ -33,24 +29,19 @@ async function generatePDF(options, config, html) {
     deviceScaleFactor: 1,
   });
 
-  // Set extra headers
   await page.setExtraHTTPHeaders({
     'Accept-Language': 'en-US',
   });
 
-  // Set content and generate PDF
   await page.setContent(html);
   const pdf = await page.pdf(config);
 
-  // Close the browser
   await browser.close();
-
   return pdf;
 }
 
-// Function to write PDF file to AWS S3
+
 function writeToS3(pdf, timestamp, res) {
-  // Prepare to write to AWS S3
   const params = {
     Key: `real_${timestamp}.pdf`,
     Body: pdf,
@@ -58,13 +49,11 @@ function writeToS3(pdf, timestamp, res) {
     ContentType: 'application/pdf',
   };
 
-  // AWS S3 configuration
   const s3 = new aws.S3({
     accessKeyId: process.env.YOUR_AWS_KEY,
     secretAccessKey: process.env.YOUR_AWS_SECRET,
   });
 
-  // Write to S3
   s3.putObject(params, function (err, response) {
     if(err) {
       console.log(err);
@@ -75,16 +64,15 @@ function writeToS3(pdf, timestamp, res) {
   });
 }
 
-// Initialize express
+
+
+
 const app = express();
 
-// Define API endpoint
 app.get("/api", async (req, res) => {
   setupPuppeteer();
-  
   let options = {};
 
-  // Set options if running on AWS Lambda
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     options = {
       args: chrome.args.concat(["--hide-scrollbars", "--disable-web-security"]),
@@ -95,7 +83,7 @@ app.get("/api", async (req, res) => {
     };
   }
 
-  // PDF configuration
+
   const bodyjson = {
     "config": {
       "width": "11.694444444444445in",
@@ -120,7 +108,6 @@ app.get("/api", async (req, res) => {
   }
   
   const { config, html } = bodyjson;
-  
   const pdf = await generatePDF(options, config, html);
   
   // Generate timestamp for unique file names
